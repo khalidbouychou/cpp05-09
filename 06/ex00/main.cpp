c8af15ff59e552771704e50ea24b9fafde668ebc
Khalid Bouychou <khbouych@e3r4p16.1337.ma>
2023-12-10 03:23:17 +0100

    cpp 05 -06

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 21:39:54 by khbouych          #+#    #+#             */
-/*   Updated: 2023/12/09 17:52:04 by khbouych         ###   ########.fr       */
+/*   Updated: 2023/12/10 03:21:44 by khbouych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int ac, char **av)
 {
     if (ac != 2)
     {
-        std::cout << "Usage: ./convert [value]" << std::endl;
+        std::cout << "invalide arguments" << std::endl;
         return (0);
     }
     ScalarConverter::convert(av[1]);
