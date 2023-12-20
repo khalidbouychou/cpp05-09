752cd7d749b101d7621d9c8c78ba261cdd55e9a0
Khalid Bouychou <khbouych@e3r7p17.1337.ma>
2023-12-20 14:47:01 +0100

    auto commit

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 02:46:00 by khbouych          #+#    #+#             */
-/*   Updated: 2023/12/13 02:51:20 by khbouych         ###   ########.fr       */
+/*   Updated: 2023/12/13 16:49:42 by khbouych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 int main()
 {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << "top " << mstack.top() << std::endl;
-    std::cout << "before pop :size stack "<< mstack.size() << std::endl;
-    mstack.pop();
-    std::cout << "after pop : size stack "<< mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+    MutantStack<char> mstack;
+    mstack.push('a');
+    mstack.push('b');
+    // mstack.push(5);
+    // mstack.push(17);
+    // std::cout << "top " << mstack.top() << std::endl;
+    // std::cout << "before pop :size stack "<< mstack.size() << std::endl;
+    // mstack.pop();
+    // std::cout << "after pop : size stack "<< mstack.size() << std::endl;
+    // mstack.push(3);
+    // mstack.push(5);
+    // mstack.push(737);
+    // mstack.push(0);
+    MutantStack<char>::iterator it = mstack.begin();
+    MutantStack<char>::iterator ite = mstack.end();
     ++it;
     --it;
     while (it != ite)
@@ -34,8 +36,6 @@ int main()
         std::cout << *it << std::endl;
         ++it;
     }
-    std::stack<int> s(mstack);
-
-    
+    // std::stack<int> s(mstack);
     return 0;
 }
